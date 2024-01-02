99fe552485659bfb5195e7b9832dee3ebc1922d7
mirnatul <www.mirnatul@gmail.com>
2024-01-02 17:19:19 +0600

    basic end of nextjs routing

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1>Welcome Home Next.js</h1>
+      <h1>Welcome Home Next.js Routing</h1>
       <h2>Links</h2>
       <ul className='bg-yellow-300 flex flex-col gap-3'>
         <li><Link href='/blog'>Blog</Link></li>
